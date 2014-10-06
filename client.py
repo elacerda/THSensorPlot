c5d0600052a23df56053c5dcefccbd83c9f2f2c1
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2014-10-06 22:09:11 +0200

    Added SQLite3 DataBase storage!

--- client.py
+++ client.py
@@ -4,6 +4,8 @@ import datetime
 import serial
 import re
 import numpy as np
+import sqlite3
+import os
 try:
     import plotly.plotly as py
     import plotly.tools as tls
@@ -12,11 +14,30 @@ except ImportError:
     print 'Could not import plotly python module.'
     sys.exit(2)
 
-try:
-    ser = serial.Serial(sys.argv[1], 9600) # Establish the connection on a specific port
-except IndexError:
-    print 'Usage: %s /dev/serial_device' % sys.argv[0]
+def connectSerial(port, baudrate = 9600):
+    try:
+        return serial.Serial(port, baudrate) # Establish the connection on a specific port
+    except IndexError:
+        print 'Usage: %s /dev/serial_device' % sys.argv[0]
+        
+def connectDB(dbfile):
+    createSQL = ''
+    if not os.path.isfile(dbfile):
+        createSQL = '''
+        CREATE TABLE THSensor(id INTEGER PRIMARY KEY, datetime TEXT, temperature REAL, humidity REAL)
+        '''        
+    db = sqlite3.connect(dbfile)
+    c = db.cursor()
+    if createSQL:
+        c.execute(createSQL)
+    return db, c
 
+def storeDataDB(db, cursor, datetime, temperature, humidity):
+    cursor.execute('''INSERT INTO THSensor(datetime, temperature, humidity) VALUES(?,?,?)''', (datetime, temperature, humidity))
+    db.commit()
+
+ser = connectSerial(sys.argv[1], 9600)
+db, cursor = connectDB('THSensor.db')
 data_payload = "";
 
 layout = Layout(
@@ -32,7 +53,6 @@ cr = py.get_credentials() # Get credentials from plotly configfile.
 trace1 = Scatter(x=[], y=[], name='temperature', stream=dict(token=cr['stream_ids'][0], maxpoints=1440)) #, xaxis='x1')
 trace2 = Scatter(x=[], y=[], name='humidity', stream=dict(token=cr['stream_ids'][1], maxpoints=1440)) #, xaxis='x2')
 fig = Figure(data=[trace1, trace2], layout=layout)
-
 py.plot(fig, filename='THSensor', fileopt='extend')
 s1 = py.Stream(cr['stream_ids'][0])
 s2 = py.Stream(cr['stream_ids'][1])
@@ -46,9 +66,14 @@ while True:
        data_payload = "";
    elif rl == ";":
        data = re.search('H([0-9]+)T([0-9]+)', data_payload)
-       print '[%s] Writing to plot.ly server...' % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-       s1.write(dict(x=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), y=np.float(data.group(2))))
-       s2.write(dict(x=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), y=np.float(data.group(1))))
+       now = datetime.datetime.now()
+       nowStr = now.strftime('%Y-%m-%d %H:%M:%S')
+       T = np.float(data.group(2))
+       H = np.float(data.group(1))
+       storeDataDB(db, cursor, nowStr, T, H)
+       print '[%s] Writing to plot.ly server...' % nowStr 
+       s1.write(dict(x=nowStr, y=T))
+       s2.write(dict(x=nowStr, y=H))
        print "%s end" % data_payload
        time.sleep(30)
    else:
