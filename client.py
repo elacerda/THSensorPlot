77617847ddb47cdeb4671760c6bd1f7f0b2ef1d3
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2014-10-06 22:55:19 +0200

    Removed unused imports and also pep8.

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import datetime
 import serial
 import re
@@ -8,43 +7,49 @@ import sqlite3
 import os
 try:
     import plotly.plotly as py
-    import plotly.tools as tls
-    from plotly.graph_objs import Scatter, Data, Figure, YAxis, Layout, Font
+    from plotly.graph_objs import Scatter, Figure, YAxis, Layout
 except ImportError:
     print 'Could not import plotly python module.'
     sys.exit(2)
 
-def connectSerial(port, baudrate = 9600):
+
+def connect_serial(port, baudrate=9600):
     try:
-        return serial.Serial(port, baudrate) # Establish the connection on a specific port
+        # Establish the connection on a specific port
+        s = serial.Serial(port, baudrate)
     except IndexError:
         print 'Usage: %s /dev/serial_device' % sys.argv[0]
-        
+
+    return s
+
+
 def connectDB(dbfile):
     createSQL = ''
-    
+
     if not os.path.isfile(dbfile):
-        createSQL = '''
-        CREATE TABLE THSensor(id INTEGER PRIMARY KEY, datetime TEXT, temperature REAL, humidity REAL)
-        '''        
-    
+        createSQL = 'CREATE TABLE THSensor(id INTEGER PRIMARY KEY, '
+        createSQL += 'datetime TEXT, temperature REAL, humidity REAL)'
+
     db = sqlite3.connect(dbfile)
     c = db.cursor()
-    
+
     if createSQL:
         c.execute(createSQL)
-    
+
     db.commit()
-    
+
     return db, c
 
+
 def storeDataDB(db, cursor, datetime, temperature, humidity):
-    cursor.execute('''INSERT INTO THSensor(datetime, temperature, humidity) VALUES(?,?,?)''', (datetime, temperature, humidity))
+    SQL = 'INSERT INTO THSensor(datetime, temperature, humidity) VALUES(?,?,?)'
+    cursor.execute(SQL, (datetime, temperature, humidity))
     db.commit()
 
-ser = connectSerial(sys.argv[1], 9600)
+
+ser = connect_serial(sys.argv[1], 9600)
 db, cursor = connectDB('THSensor.db')
-data_payload = "";
+data_payload = ''
 
 layout = Layout(
     title='THSensor v0.1',
@@ -54,10 +59,12 @@ layout = Layout(
     ),
 )
 
-cr = py.get_credentials() # Get credentials from plotly configfile.
-
-trace1 = Scatter(x=[], y=[], name='temperature', stream=dict(token=cr['stream_ids'][0], maxpoints=1440)) #, xaxis='x1')
-trace2 = Scatter(x=[], y=[], name='humidity', stream=dict(token=cr['stream_ids'][1], maxpoints=1440)) #, xaxis='x2')
+# Get credentials from plotly configfile.
+cr = py.get_credentials()
+trace1 = Scatter(x=[], y=[], name='temperature',
+                 stream=dict(token=cr['stream_ids'][0], maxpoints=1440))
+trace2 = Scatter(x=[], y=[], name='humidity',
+                 stream=dict(token=cr['stream_ids'][1], maxpoints=1440))
 fig = Figure(data=[trace1, trace2], layout=layout)
 py.plot(fig, filename='THSensor', fileopt='extend')
 s1 = py.Stream(cr['stream_ids'][0])
@@ -66,20 +73,21 @@ s1.open()
 s2.open()
 
 while True:
-   rl = ser.readline().strip() # Read the newest output from the Arduino
+    # Read the newest output from the Arduino
+    rl = ser.readline().strip()
 
-   if rl == ":":
-       data_payload = "";
-   elif rl == ";":
-       data = re.search('H([0-9]+)T([0-9]+)', data_payload)
-       now = datetime.datetime.now()
-       nowStr = now.strftime('%Y-%m-%d %H:%M:%S')
-       T = np.float(data.group(2))
-       H = np.float(data.group(1))
-       storeDataDB(db, cursor, nowStr, T, H)
-       print '[%s] Writing to plot.ly server...' % nowStr 
-       s1.write(dict(x=nowStr, y=T))
-       s2.write(dict(x=nowStr, y=H))
-       print "%s end" % data_payload
-   else:
-       data_payload += rl
+    if rl == ':':
+        data_payload = ''
+    elif rl == ';':
+        data = re.search('H([0-9]+)T([0-9]+)', data_payload)
+        now = datetime.datetime.now()
+        nowStr = now.strftime('%Y-%m-%d %H:%M:%S')
+        T = np.float(data.group(2))
+        H = np.float(data.group(1))
+        storeDataDB(db, cursor, nowStr, T, H)
+        print '[%s] Writing to plot.ly server...' % nowStr
+        s1.write(dict(x=nowStr, y=T))
+        s2.write(dict(x=nowStr, y=H))
+        print '%s end' % data_payload
+    else:
+        data_payload += rl
